ce143c1f819c8737783e99e152a811944615122b
chingiz.akmatov <chingiz.akmatov@workhuman.com>
2022-09-05 11:08:43 +0400

    updated fmx

--- rabbitmq-retry-fmx/src/main/java/com/example/rabbitmqretryfmx/listeners/RabbitMQListener.java
+++ rabbitmq-retry-fmx/src/main/java/com/example/rabbitmqretryfmx/listeners/RabbitMQListener.java
@@ -1,5 +1,7 @@
 package com.example.rabbitmqretryfmx.listeners;
 
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.ImmediateRequeueAmqpException;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -20,5 +22,11 @@ public class RabbitMQListener {
             ))
     public void listenMessageQueue(Message<String> in) {
         System.out.println(in.getHeaders());
+        boolean redelivered = (boolean) in.getHeaders().get("amqp_redelivered");
+        System.out.println("REDELIVERED: " + redelivered);
+        if (!redelivered) {
+            throw new AmqpRejectAndDontRequeueException("Don't retry");
+        }
+        throw new ImmediateRequeueAmqpException("Retry again");
     }
 }
